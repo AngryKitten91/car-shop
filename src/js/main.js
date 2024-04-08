710eb6923cd8cd6dadd92ae15fb316422932cb3f
Bart <bartkof91@gmail.com>
2024-04-08 22:29:13 +0200

    car list styled

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@
 // import LOCALSTORAGE from "./_utils.js";
 // import DATA_NAME from "./_data.js";
 
+const $cars = document.querySelector("#cars");
+
 const getCars = async () => {
   try {
-    const data = await fetch("data.json");
+    const data = await fetch("assets/data.json");
     const cars = await data.json();
     return cars;
   } catch (error) {
@@ -12,4 +14,30 @@ const getCars = async () => {
   }
 };
 
-getCars().then((val) => console.log(val));
+const carSchema = (
+  producer,
+  year_of_production,
+  model,
+  horse_power,
+  mileage_km
+) => {
+  return `
+  <div class="car">
+    <div class="car-name"><h1>${producer}</h1></div>
+    <div class="car-name">${model}</div>
+    <div class="car-name">${year_of_production}</div>
+    <div class="car-name">${horse_power} HP</div>
+    <div class="car-name">${mileage_km} km</div>
+  </div>`;
+};
+
+getCars().then(({ cars }) => {
+  cars.map(
+    ({ producer, year_of_production, model, horse_power, mileage_km }) => {
+      $cars.insertAdjacentHTML(
+        "beforeend",
+        carSchema(producer, year_of_production, model, horse_power, mileage_km)
+      );
+    }
+  );
+});
